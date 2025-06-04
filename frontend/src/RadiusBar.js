96dc6cd7e11aa15adf45385276148e76bab179d6
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2025-06-04 21:19:44 +0530

    first commit

--- frontend/src/RadiusBar.js
+++ frontend/src/RadiusBar.js
@@ -1,39 +1,46 @@
 import React from 'react';
-import { RadialBarChart,RadialBar,Legend,Tooltip } from 'recharts';
+import { RadialBarChart, RadialBar, Legend, Tooltip, ResponsiveContainer } from 'recharts';
 
 function RadiusBar(props) {
- const data = [
-  {
-    "name": props.bowlingTeam,
-    "uv": props.target,
-    "fill": "#82ca9d"
-  },
-  {
-    "name": props.battingTeam,
-    "uv": props.score,
-    "fill": "#8884d8"
-  }
-]   
+  const data = [
+    {
+      name: props.bowlingTeam,
+      uv: props.target,
+      fill: '#82ca9d',
+    },
+    {
+      name: props.battingTeam,
+      uv: props.score,
+      fill: '#8884d8',
+    },
+  ];
+
   return (
-<RadialBarChart 
-  width={350} 
-  height={300} 
-  innerRadius="40%" 
-  outerRadius="110%" 
-  data={data} 
-  startAngle={180} 
-  endAngle={0}
->
- <RadialBar
-    minAngle={15}
-    background
-    clockWise={true}
-    dataKey="uv"
-    label={{ position: 'OutsideEnd', fill: '#000', formatter: (value) => `${value}` }}
-  />
-  <Legend/>
-  <Tooltip />
-</RadialBarChart>
+    <div style={{ width: '100%', height: 300 }}>
+      <ResponsiveContainer>
+        <RadialBarChart
+          innerRadius="30%"
+          outerRadius="80%"
+          data={data}
+          startAngle={180}
+          endAngle={0}
+        >
+          <RadialBar
+            minAngle={15}
+            background
+            clockWise
+            dataKey="uv"
+            label={{
+              position: 'OutsideEnd',
+              fill: '#000',
+              formatter: (value) => `${value}`,
+            }}
+          />
+          <Legend iconSize={10} layout="horizontal" verticalAlign="bottom" align="center" />
+          <Tooltip />
+        </RadialBarChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
 
